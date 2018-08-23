4fb1db6c4f6c0c51b6823abe299f133fe465e7f4
gustavo1017 <gustavoeqr1017@gmail.com>
2018-08-23 15:04:04 -0500

    Timer funcion setInterval

--- js/main.js
+++ js/main.js
@@ -1,41 +1,18 @@
 'use strict'
 // envento load para que cuando cargue todo el cuerpo de la pagina se introdusca el javascript
 window.addEventListener('load', () => {
-	//enventos del teclado
+	// Timers
 
-    var boton = document.querySelector("#boton");
-
-//focus
-    var input = document.querySelector("#campo_nombre");
-	input.addEventListener('focus', function(){
-		console.log("Estas haciendo focus");
-	});
-
-//Blur
-    input.addEventListener('blur', function(){
-		console.log("Estas fuera del input");
-	});
-
-
-//keydown
-    input.addEventListener('keydown', function(event){
-		console.log("pulsas tecla ", String.fromCharCode(event.keyCode));
+	var tiempo = setInterval(() =>{
+		console.log("Set interval ejecutado");
+		var encabezado = document.querySelector("h1")
+		if(encabezado.style.fontSize == "50px"){
+			encabezado.style.fontSize ="30px";
+		}else{
+			encabezado.style.fontSize = "50px";
+		}
 		
-	});
-
-//keypress
-    input.addEventListener('keypress', function(event){
-		console.log("tecla presionada ", String.fromCharCode(event.keyCode));
-		
-	});
-
-//keyup
-    input.addEventListener('keyup', function(event){
-		console.log("tecla soltada ", String.fromCharCode(event.keyCode));
-		
-	});
-
-
+	},5000);
 
 });
 
