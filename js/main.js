a1b584459b731954410e4bd25ff131338040ed79
gustavo1017 <gustavoeqr1017@gmail.com>
2018-08-23 14:52:22 -0500

    Evnetos del teclado

--- js/main.js
+++ js/main.js
@@ -1,37 +1,34 @@
 'use strict'
-//enventos del raton
-//
-
-
-function cambiarColor(){
-	console.log("me has dado click");
-	var bg = boton.style.background;
-	if(bg =="green"){
-		boton.style.background ="red";
-
-	}else{
-		boton.style.background = "green";
-	}
-		boton.style.padding ="10px";
-		boton.style.border = "1px solid #ccc";
-	return true;
-
-}
+//enventos del teclado
 
 var boton = document.querySelector("#boton");
-//capturar evento del boton
-boton.addEventListener('click',function(){
-	cambiarColor();
-});
 
-//Mouse over
-boton.addEventListener('mouseover', function(){
-	boton.style.background = "yellow";
+//focus
+var input = document.querySelector("#campo_nombre");
+	input.addEventListener('focus', function(){
+		console.log("Estas haciendo focus");
+	});
 
-});
+//Blur
+input.addEventListener('blur', function(){
+		console.log("Estas fuera del input");
+	});
 
-//Mouseout
-boton.addEventListener('mouseout', function(){
-	boton.style.background = "#ccc";
-});
 
+//keydown
+input.addEventListener('keydown', function(event){
+		console.log("pulsas tecla ", String.fromCharCode(event.keyCode));
+		
+	});
+
+//keypress
+input.addEventListener('keypress', function(event){
+		console.log("tecla presionada ", String.fromCharCode(event.keyCode));
+		
+	});
+
+//keyup
+input.addEventListener('keyup', function(event){
+		console.log("tecla soltada ", String.fromCharCode(event.keyCode));
+		
+	});
