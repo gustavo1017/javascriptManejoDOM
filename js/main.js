5164890df32efd5fa51f15893f68a97b35b26874
gustavo1017 <gustavoeqr1017@gmail.com>
2018-08-22 21:48:03 -0500

    redireccionando pasando url

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@
 // BOM - Browser Object Model
 //Propiedades para saber el ancho y el alto del navegador
 function getBom(){
-	console.log(window.innerHeight);
-	console.log(window.innerWidth);
+	console.log(screen.Height);
+	console.log(screen.Width);
+	console.log(window.location.href);
 }
+
+//redireccionar a otra pagina pasandole el url
+function redirect(url){
+	window.location.href = url;
+}
+
 getBom();
 
 
