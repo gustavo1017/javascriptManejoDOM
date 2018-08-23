5cb278f7b90fbbf13b0fb20b7e5b1c195b535e97
gustavo1017 <gustavoeqr1017@gmail.com>
2018-08-23 14:57:10 -0500

    Evento Load

--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
 'use strict'
-//enventos del teclado
+// envento load para que cuando cargue todo el cuerpo de la pagina se introdusca el javascript
+window.addEventListener('load', () => {
+	//enventos del teclado
 
-var boton = document.querySelector("#boton");
+    var boton = document.querySelector("#boton");
 
 //focus
-var input = document.querySelector("#campo_nombre");
+    var input = document.querySelector("#campo_nombre");
 	input.addEventListener('focus', function(){
 		console.log("Estas haciendo focus");
 	});
 
 //Blur
-input.addEventListener('blur', function(){
+    input.addEventListener('blur', function(){
 		console.log("Estas fuera del input");
 	});
 
 
 //keydown
-input.addEventListener('keydown', function(event){
+    input.addEventListener('keydown', function(event){
 		console.log("pulsas tecla ", String.fromCharCode(event.keyCode));
 		
 	});
 
 //keypress
-input.addEventListener('keypress', function(event){
+    input.addEventListener('keypress', function(event){
 		console.log("tecla presionada ", String.fromCharCode(event.keyCode));
 		
 	});
 
 //keyup
-input.addEventListener('keyup', function(event){
+    input.addEventListener('keyup', function(event){
 		console.log("tecla soltada ", String.fromCharCode(event.keyCode));
 		
 	});
+
+
+
+});
+
+
+
